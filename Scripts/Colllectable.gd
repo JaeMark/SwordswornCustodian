2261b851946b6d3cc79b981cdd2352843507d88b
Jetro de Leon <j08mark@hotmail.com>
2023-10-11 14:03:48 -0500

    Added collectable type enums to the collectables

--- Scripts/Colllectable.gd
+++ Scripts/Colllectable.gd
@@ -1,8 +1,17 @@
 extends Area3D
 
+class_name Collectable
+
 @export var rotation_speed = 1.0
+@export var collectableType = CollectableType.BlueShield
 
 @onready var mesh = $MeshInstance3D
+
+enum CollectableType {
+	BlueShield,
+	RedShield,
+	Sword
+}
 
 func _on_body_entered(body):
 	self.queue_free()
