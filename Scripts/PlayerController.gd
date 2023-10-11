a546cfaf34c8a0446033e12b5310c7ed91477f64
Jetro De Leon <j08mark@hotmail.com>
2023-10-10 23:04:06 -0500

    Made jump snappier

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,12 +1,12 @@
 extends CharacterBody3D
 
-@export var speed : = 10.0
-@export var acceleration : = 4.0
-@export var jump_speed : = 8.0
-@export var rotation_speed : = 5.0
-@export var camera_lag : = 0.1 # Adjust this value to control the camera lag
+@export var speed = 10.0
+@export var acceleration = 4.0
+@export var jump_speed = 12.0
+@export var max_jump_height = 5.0 
+@export var rotation_speed = 5.0
+@export var camera_lag = 0.1
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var spring_arm = $SpringArm3D
@@ -14,23 +14,31 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var target_rotation = 0.0
 var target_spring_rotation = 0.0
+var jump_start_height = 0.0
+var is_jumping = false
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if not is_on_floor() and not is_jumping:
+		velocity.y -= gravity * jump_speed * delta
+	elif Input.is_action_just_pressed("jump"):
+		is_jumping = true
+		jump_start_height = global_transform.origin.y
 		velocity.y = jump_speed
 
-	# Handle rotation based on input.
+	if is_jumping and Input.is_action_just_released("jump"):
+		is_jumping = false
+		
+	if is_jumping:
+		var jump_height = abs(global_transform.origin.y - jump_start_height)
+		if jump_height >= max_jump_height:
+			velocity.y = 0
+			is_jumping = false
+
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
 
-	# Rotate the player left or right
 	if input_dir.x != 0:
-		model.rotation.y -= input_dir.x * rotation_speed * delta # <- Changed the sign here
-		target_spring_rotation -= input_dir.x * rotation_speed * delta # <- Changed the sign here
+		model.rotation.y -= input_dir.x * rotation_speed * delta
+		target_spring_rotation -= input_dir.x * rotation_speed * delta
 
 	spring_arm.rotation.y = lerp_angle(spring_arm.rotation.y, target_spring_rotation, camera_lag)
 
@@ -38,13 +46,10 @@ func _physics_process(delta):
 	move_and_slide()
 
 func handle_movement_input(delta):
-	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
 
-	# Calculate the movement direction based on player's rotation
 	var direction = Vector3(0, 0, 1).rotated(Vector3.UP, model.rotation.y)
 	
-	# Adjust the velocity based on the input
 	var velocity_y = velocity.y;
 	velocity = lerp(velocity, direction * input_dir.y * speed, acceleration * delta)
 	velocity.y = velocity_y
