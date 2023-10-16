e5fd2f0600000842aedd58856db8a166282187eb
Jetro de Leon <j08mark@hotmail.com>
2023-10-16 11:47:52 -0500

    King now looks at the player

--- Scripts/KingController.gd
+++ Scripts/KingController.gd
@@ -3,10 +3,13 @@ extends Node3D
 @export var required_blue_shield = 2
 @export var required_red_shield = 1
 @export var required_sword = 3
+@export var player : CharacterBody3D
 
 @onready var king_ui = $SubViewport/KingRequestUI
+@onready var king_mesh = $KingMesh
 
 var is_request_fulfilled := false;
+var is_player_in_sight := false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,4 +20,13 @@ func _ready():
 func check_request(num_blue_shield : int, num_red_shield : int, num_sword : int) :
 	is_request_fulfilled = num_blue_shield >= required_blue_shield && num_red_shield >= required_red_shield && num_sword >= required_sword
 
+func _process(delta):
+	if is_player_in_sight && player:
+		king_mesh.look_at(player.position, Vector3(0,1,0))
 
+func _on_area_3d_body_entered(body):
+	is_player_in_sight = true
+	
+
+func _on_area_3d_body_exited(body):
+	is_player_in_sight = false
